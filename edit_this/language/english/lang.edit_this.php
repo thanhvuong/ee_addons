97b048feb4c2af1671c4ece79dfa23d353ba5748
Thanh Vuong <dev@vuongs.org>
2011-09-08 15:15:00 -0400

    first commit

---
+++ edit_this/language/english/lang.edit_this.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang = array(
+	'edit' =>
+	'Edit',
+
+	'cp_url' =>
+	'URL to your Control Panel index page',
+
+	'cp_imgurl' =>
+	'URL to the image that you like to use (Optional)',
+
+
+// END
+''=>''
+);
+
+?>
