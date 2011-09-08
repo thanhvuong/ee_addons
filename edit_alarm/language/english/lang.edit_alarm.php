8fdd20fc0932d62289c2c702206ef727c467fd63
Thanh Vuong <dev@vuongs.org>
2011-09-08 15:19:20 -0400

    first commit

---
+++ edit_alarm/language/english/lang.edit_alarm.php
@@ -0,0 +1,50 @@
+<?php
+$lang = array(
+
+'apply_to' =>
+'Display Edit Alerts for:',
+
+'expire' =>
+'Alert expiration (in minutes)',
+
+'disable_submit' =>
+'If in edit, disable submit button for entries and templates?',
+
+'alert_css' =>
+'CSS for Alert Div',
+
+'channel_entries' =>
+'Channel Entries',
+
+'templates' =>
+'Templates',
+
+'wiki_articles' =>
+'Wiki Articles',
+
+'currently_editing' =>
+'Currently editing this %type%: ',	
+
+'entry' =>
+'entry',
+
+'template' =>
+'template',
+
+'wiki_article' =>
+'article',
+
+'less_than_one' =>
+'&lt; 1',
+
+'minute_ago' =>
+'minute ago',
+
+'minutes_ago' =>
+'minutes ago',
+
+'inedit_msg' =>
+'Another user is editing this resource. Submit button has been disabled. Please try again later.',
+
+// IGNORE
+''=>'');?>
